be399ac2c28ccd169be39728c84f1c38a8acb2df
mvneerven <marc@van-neerven.net>
2021-05-17 11:09:20 +0200

    npm

---
+++ js/xo.js
@@ -0,0 +1,29 @@
+import ExoFormFactory from '../src/exo/ExoFormFactory';
+import ExoRouteModule from '../src/exo/ExoRouteModule';
+import ExoWizardRouteModule from '../src/exo/ExoWizardRouteModule';
+import ExoBaseControls from '../src/exo/ExoBaseControls';
+import DOM from '../src/pwa/DOM';
+import PWA from '../src/pwa/PWA';
+import Core from '../src/pwa/Core';
+import MsIdentity from '../src/pwa/MsIdentity';
+
+const xo = {
+    core: Core,
+    dom: DOM,
+    pwa: PWA,
+    route: ExoRouteModule,
+    form: {
+        factory: ExoFormFactory,
+        fields: {
+            base: ExoBaseControls
+        },        
+        wizard: ExoWizardRouteModule
+    },
+    identity: {
+        msal: MsIdentity
+    }
+};
+
+window.xo = xo;
+
+export default xo;
